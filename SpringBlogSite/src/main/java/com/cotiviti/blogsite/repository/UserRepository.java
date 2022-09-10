87929ccf44b4ea9d20bf06f5ed6dcb79536651d9
N-itish <manandharnara@gmail.com>
2022-09-10 19:39:47 +0545

    Delete SpringBlogSite directory

--- SpringBlogSite/src/main/java/com/cotiviti/blogsite/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cotiviti.blogsite.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.cotiviti.blogsite.entities.User;
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
